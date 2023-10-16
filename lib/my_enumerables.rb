d0258526333d799848dd3d1cc65d5f2a7e234af4
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-10-16 21:44:30 +0200

    my_each, my_count, my_each_with_index: done

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,12 +7,28 @@ end
 # your enumerable module will have access
 # to this method
 class Array
+
   # Define my_each here
   def my_each
-    self.each {|x| yield x}
+    self.each {|element| yield element}
   end
+
   # Define my_count
   def my_count
-    yield self.count
+    count = 0
+    if block_given?
+      self.my_each {|x| count += 1 if yield x} 
+    else
+      count = self.length
+    end
+    count
+  end
+
+  def my_each_with_index
+    i = 0
+    self.each do |x|
+      yield x, i
+      i += 1
+    end
   end
 end
