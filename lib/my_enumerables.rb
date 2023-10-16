30b469f905c91869ace5235cb28eb61e15eb5bbe
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2023-10-16 22:41:20 +0200

    Moved methods to the module

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,18 +1,5 @@
 module Enumerable
   # Your code goes here
-end
-
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
-class Array
-  # Define my_each here
-  def my_each
-    each {|element| yield element}
-  end
-
-  # Define my_count
   def my_count
     count = 0
     if block_given?
@@ -25,7 +12,7 @@ class Array
 
   def my_each_with_index
     i = 0
-    self.my_each do |x|
+    my_each do |x|
       yield x, i
       i += 1
     end
@@ -60,4 +47,16 @@ class Array
     my_each { |x| mapped << block.call(x)}
     mapped
   end
+end
+
+# You will first have to define my_each
+# on the Array class. Methods defined in
+# your enumerable module will have access
+# to this method
+class Array
+  # Define my_each here
+  def my_each(&block)
+    each {|element| block.call(element)}
+  end
+  include Enumerable
 end
